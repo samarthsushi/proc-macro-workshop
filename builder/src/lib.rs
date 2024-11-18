3b38fde058f267f0e40bd36d8afe063bffbb66ff
samarth <samarthkulkarni@icloud.com>
2024-11-19 00:20:52 +0530

    feeling sleepy

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,60 @@
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
+use quote::quote;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = &input.ident;
+    let builder_name = format!("{}Builder", name);
+    let builder_ident = syn::Ident::new(&builder_name, name.span());
+    let expanded = quote! {
+        struct #builder_ident {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+        impl #builder_ident {
+            fn executable(&mut self, executable: String) -> &mut Self {
+                self.executable = Some(executable);
+                self
+            }
 
-    unimplemented!()
+            fn args(&mut self, args: Vec<String>) -> &mut Self {
+                self.args = Some(args);
+                self
+            }
+
+            fn env(&mut self, env: Vec<String>) -> &mut Self {
+                self.env = Some(env);
+                self
+            }
+
+            fn current_dir(&mut self, current_dir: String) -> &mut Self {
+                self.current_dir = Some(current_dir);
+                self
+            }   
+
+            fn build(&mut self) -> Result<#name, Box<dyn std::error::Error>> {
+                Ok (#name {
+                    executable: self.executable.clone().ok_or("executable is not set")?,
+                    args: self.args.clone().ok_or("args is not set")?,
+                    env: self.env.clone().ok_or("env is not set")?,
+                    current_dir: self.current_dir.clone().ok_or("current_dir is not set")?,
+                })
+            }
+        }
+        impl #name {
+            fn builder() -> #builder_ident {
+                #builder_ident {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+    };
+    TokenStream::from(expanded)
 }
