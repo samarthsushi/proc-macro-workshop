e2674dde94929f21256dd764a2fd6a8d008a9594
samarth <samarthkulkarni@icloud.com>
2024-11-19 17:41:48 +0530

    builder can work for any struct

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -4,44 +4,50 @@ use quote::quote;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as DeriveInput);
-    let name = &input.ident;
+    let ast = parse_macro_input!(input as DeriveInput);
+    let name = &ast.ident;
     let builder_name = format!("{}Builder", name);
     let builder_ident = syn::Ident::new(&builder_name, name.span());
+    let fields = if let syn::Data::Struct(syn::DataStruct { 
+        fields: syn::Fields::Named(syn::FieldsNamed {ref named, ..}), 
+        ..
+    }) = ast.data {
+        named
+    } else {
+        unimplemented!();
+    };
+
+    let fields_after_option_types = fields.iter().map(|f| {
+        let field_name = &f.ident;
+        let ty = &f.ty;
+        quote! { #field_name: std::option::Option<#ty> }
+    });
+    let setter_methods = fields.iter().map(|f| {
+        let field_name = &f.ident;
+        let ty = &f.ty;
+        quote! {
+            fn #field_name(&mut self, #field_name: #ty) -> &mut Self {
+                self.#field_name = Some(#field_name);
+                self
+            }
+        }
+    });
+    let build_method = fields.iter().map(|f| {
+        let field_name = &f.ident;
+        quote! {
+            #field_name: self.#field_name.clone().ok_or(concat!(stringify!(#field_name), "is not set"))?
+        }
+    });
     let expanded = quote! {
         struct #builder_ident {
-            executable: Option<String>,
-            args: Option<Vec<String>>,
-            env: Option<Vec<String>>,
-            current_dir: Option<String>,
+            #(#fields_after_option_types,)*
         }
         impl #builder_ident {
-            fn executable(&mut self, executable: String) -> &mut Self {
-                self.executable = Some(executable);
-                self
-            }
+            #(#setter_methods)*
 
-            fn args(&mut self, args: Vec<String>) -> &mut Self {
-                self.args = Some(args);
-                self
-            }
-
-            fn env(&mut self, env: Vec<String>) -> &mut Self {
-                self.env = Some(env);
-                self
-            }
-
-            fn current_dir(&mut self, current_dir: String) -> &mut Self {
-                self.current_dir = Some(current_dir);
-                self
-            }   
-
-            fn build(&mut self) -> Result<#name, Box<dyn std::error::Error>> {
+            fn build(&self) -> Result<#name, Box<dyn std::error::Error>> {
                 Ok (#name {
-                    executable: self.executable.clone().ok_or("executable is not set")?,
-                    args: self.args.clone().ok_or("args is not set")?,
-                    env: self.env.clone().ok_or("env is not set")?,
-                    current_dir: self.current_dir.clone().ok_or("current_dir is not set")?,
+                    #(#build_method,)*
                 })
             }
         }
