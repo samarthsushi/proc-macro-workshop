91a3dad2ad38aeb6e5a69cc3c0e8b42bd045fb7f
samarth <samarthkulkarni@icloud.com>
2024-11-21 18:51:27 +0530

    git wtf

--- builder/tests/progress.rs
+++ builder/tests/progress.rs
@@ -8,11 +8,6 @@ fn tests() {
     // t.pass("tests/05-method-chaining.rs");
     // t.pass("tests/06-optional-field.rs");
     // t.pass("tests/07-repeated-field.rs");
-<<<<<<< HEAD
     //t.compile_fail("tests/08-unrecognized-attribute.rs");
     t.pass("tests/09-redefined-prelude-types.rs");
-=======
-    t.compile_fail("tests/08-unrecognized-attribute.rs");
-    //t.pass("tests/09-redefined-prelude-types.rs");
->>>>>>> a09bb46030ca8f4dd8b6d47657fc876193ff5ce6
 }
